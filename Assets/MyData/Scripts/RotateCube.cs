440168af7a6ad969ecea6e83b6699cc35c015168
Noah <nplotkin@oberlin.edu>
2019-11-03 16:45:54 -0500

    first commit

---
+++ Assets/MyData/Scripts/RotateCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float spinforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinforce * Time.deltaTime, 0);
+    }
+
+    public void changeSpin()
+    {
+        spinforce = -spinforce;
+    }
+
+
+}
